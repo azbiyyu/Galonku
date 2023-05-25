16b80d1ddbc7893d0e4c44c6b88ba0b63793fcbd
irfan <iramadhani679@gmail.com>
2023-05-25 21:10:47 +0700

    nambah google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +10,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDbytIzLZ2ahM3Ukjk6W0-jjaL83xpTWD0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyDbytIzLZ2ahM3Ukjk6W0-jjaL83xpTWD0"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+@end
 }
--- lib/DepotPage/home_page_user.dart
+++ lib/DepotPage/home_page_user.dart
@@ -1,15 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:galonku/DepotPage/chat_user.dart';
 
 class HomePageUser extends StatefulWidget {
-  const HomePageUser({super.key});
+  const HomePageUser({Key? key}) : super(key: key);
+
   static const nameRoute = '/homepageuser';
+
   @override
   State<HomePageUser> createState() => _HomePageUserState();
 }
 
 class _HomePageUserState extends State<HomePageUser> {
-  List widgetPage = [
+  List<Widget> widgetPage = [
     Center(
       child: Text(
         "Home",
@@ -22,12 +27,7 @@ class _HomePageUserState extends State<HomePageUser> {
         style: TextStyle(fontSize: 50),
       ),
     ),
-    Center(
-      child: Text(
-        "Maps",
-        style: TextStyle(fontSize: 50),
-      ),
-    ),
+    GoogleMapPage(),
     Center(
       child: ChatUser(),
     ),
@@ -54,12 +54,11 @@ class _HomePageUserState extends State<HomePageUser> {
       bottomNavigationBar: SizedBox(
         height: 95,
         child: BottomNavigationBar(
-          currentIndex: index, // set up index berapa yang mau muncul duluan
-          selectedItemColor: Colors.blue[400], // set up kalau lagi aktif
+          currentIndex: index,
+          selectedItemColor: Colors.blue[400],
           unselectedIconTheme: IconThemeData(color: Colors.black26),
-          unselectedItemColor:
-              Color.fromARGB(255, 0, 0, 0), // set up kalau lagi ga aktif
-          iconSize: 30, //default 24
+          unselectedItemColor: Color.fromARGB(255, 0, 0, 0),
+          iconSize: 30,
           onTap: (value) {
             setState(() {
               index = value;
@@ -70,13 +69,15 @@ class _HomePageUserState extends State<HomePageUser> {
             BottomNavigationBarItem(
                 icon: Image.asset('images/Icon_HomePage.png'), label: 'Home'),
             BottomNavigationBarItem(
-                icon: Image.asset('images/Icon_Pesanan.png'), label: "Pesanan"),
+                icon: Image.asset('images/Icon_Pesanan.png'),
+                label: "Pesanan"),
             BottomNavigationBarItem(
                 icon: Image.asset('images/Icon_Maps.png'), label: "Maps"),
             BottomNavigationBarItem(
                 icon: Image.asset('images/Icon_Pesan.png'), label: "Pesan"),
             BottomNavigationBarItem(
-                icon: Image.asset('images/Icon_Setting.png'), label: "Setting"),
+                icon: Image.asset('images/Icon_Setting.png'),
+                label: "Setting"),
           ],
         ),
       ),
@@ -84,8 +85,47 @@ class _HomePageUserState extends State<HomePageUser> {
   }
 }
 
-/* 
-            if(index == 0){
-              navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()),),
-            }
-          */
+class GoogleMapPage extends StatefulWidget {
+  @override
+  _GoogleMapPageState createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
