9a912f0e92b9b6f92d36ac4931a11ae03d8bd7ee
irfan <iramadhani679@gmail.com>
2023-05-25 21:24:47 +0700

    edit google maps

--- lib/DepotPage/home_page_user.dart
+++ lib/DepotPage/home_page_user.dart
@@ -95,13 +95,13 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(-6.982563, 110.409396),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-    bearing: 192.8334901395799,
-    target: LatLng(37.43296265331129, -122.08832357078792),
+  static const CameraPosition _current = CameraPosition(
+    bearing: 90,
+    target: LatLng(-6.982563, 110.409396),
     tilt: 59.440717697143555,
     zoom: 19.151926040649414,
   );
@@ -110,7 +110,7 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.terrain,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
@@ -118,14 +118,14 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        label: const Text('Pusatkan'),
+        icon: const Icon(Icons.map_sharp),
       ),
     );
   }
 
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_current));
   }
 }
