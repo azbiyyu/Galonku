e2dc1eb3537941c5ab097d395dd5b05cc8a225bb
irfan <iramadhani679@gmail.com>
2023-06-25 22:05:12 +0700

    otp_auth

--- lib/LoginPage/verifikasi.dart
+++ lib/LoginPage/verifikasi.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
-import 'package:galonku/DepotPage/home_page_depot.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:galonku/DepotPage/home_page_user.dart';
 import 'package:galonku/LoginPage/mitra_input.dart';
 
@@ -15,7 +17,97 @@ class Verifikasi extends StatefulWidget {
 }
 
 class _VerifikasiState extends State<Verifikasi> {
+  String _phoneNumber = "";
   String _otpCode = "";
+  String _verificationId = "";
+  bool _isPhoneNumberInputted = false;
+
+  Future<void> _verifyPhoneNumber() async {
+    // ignore: prefer_function_declarations_over_variables
+    PhoneVerificationCompleted verificationCompleted =
+        (PhoneAuthCredential credential) async {
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      if (widget.isFromUserSignIn) {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MitraInput()),
+        );
+      } else {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePageUser()),
+        );
+      }
+    };
+
+    // ignore: prefer_function_declarations_over_variables
+    PhoneVerificationFailed verificationFailed =
+        (FirebaseAuthException e) {
+      print('Phone number verification failed. Code: ${e.code}. Message: ${e.message}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Phone number verification failed')),
+      );
+    };
+
+    // ignore: prefer_function_declarations_over_variables
+    PhoneCodeSent codeSent =
+        (String verificationId, int? resendToken) async {
+      setState(() {
+        _verificationId = verificationId;
+      });
+    };
+
+    // ignore: prefer_function_declarations_over_variables
+    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      setState(() {
+        _verificationId = verificationId;
+      });
+    };
+
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: _phoneNumber,
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to verify phone number. Error: $e')),
+      );
+    }
+  }
+
+  Future<void> _signInWithPhoneNumber() async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId,
+        smsCode: _otpCode,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      if (widget.isFromUserSignIn) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MitraInput()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePageUser()),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to sign in with phone number. Error: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +129,7 @@ class _VerifikasiState extends State<Verifikasi> {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          "Verifikasi Kode OTP",
+                          "Phone Authentication",
                           style: TextStyle(
                             fontSize: 24.0,
                             fontWeight: FontWeight.bold,
@@ -45,7 +137,7 @@ class _VerifikasiState extends State<Verifikasi> {
                         ),
                         SizedBox(height: 16.0),
                         Text(
-                          "Masukkan kode OTP yang telah kami kirim ke nomor anda:",
+                          "Enter your phone number to verify:",
                           style: TextStyle(
                             fontSize: 16.0,
                           ),
@@ -53,40 +145,75 @@ class _VerifikasiState extends State<Verifikasi> {
                         SizedBox(height: 16.0),
                         TextField(
                           decoration: InputDecoration(
-                            labelText: "Kode OTP",
+                            labelText: "Phone Number (+62)",
                             border: OutlineInputBorder(),
                           ),
-                          keyboardType: TextInputType.number,
-                          maxLength: 6,
+                          keyboardType: TextInputType.phone,
                           onChanged: (value) {
                             setState(() {
-                              _otpCode = value;
+                              _phoneNumber = value;
                             });
                           },
                         ),
                         SizedBox(height: 16.0),
                         ElevatedButton(
-                          onPressed: _otpCode.length == 6
-                              ? () {
-                                  if (widget.isFromUserSignIn) {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => MitraInput()),
-                                    );
-                                  } else {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => HomePageUser()),
-                                    );
-                                  }
-                                }
-                              : null,
+                          onPressed: () {
+                            if (_phoneNumber.isNotEmpty) {
+                              setState(() {
+                                _isPhoneNumberInputted = true;
+                              });
+                              _verifyPhoneNumber();
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Please enter a phone number')),
+                              );
+                            }
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color.fromARGB(255, 52, 83, 209),
                           ),
-                          child: Text("Verifikasi"),
+                          child: Text("Verify Phone Number"),
+                        ),
+                        SizedBox(height: 16.0),
+                        Visibility(
+                          visible: _isPhoneNumberInputted,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                "Enter the OTP code:",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                              SizedBox(height: 16.0),
+                              TextField(
+                                decoration: InputDecoration(
+                                  labelText: "OTP Code",
+                                  border: OutlineInputBorder(),
+                                ),
+                                keyboardType: TextInputType.number,
+                                maxLength: 6,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _otpCode = value;
+                                  });
+                                },
+                              ),
+                              SizedBox(height: 16.0),
+                              ElevatedButton(
+                                onPressed: _otpCode.length == 6
+                                    ? () {
+                                        _signInWithPhoneNumber();
+                                      }
+                                    : null,
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color.fromARGB(255, 52, 83, 209),
+                                ),
+                                child: Text("Verify OTP"),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
