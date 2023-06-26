a5d8f6053755ba86216898259165af14c00bf8d6
azbiyyu <azmiabiyyudzaky456@gmail.com>
2023-06-26 11:09:56 +0700

    nambah settings

--- lib/DepotPage/settings_depot.dart
+++ lib/DepotPage/settings_depot.dart
@@ -1,6 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:galonku/LandingPage/login_role.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsDepot extends StatefulWidget {
@@ -12,82 +15,231 @@ class SettingsDepot extends StatefulWidget {
 }
 
 class _SettingDepotState extends State<SettingsDepot> {
+  List<String> images = [
+    'images/test_foto.png',
+    'images/test_foto.png',
+    'images/test_foto.png',
+  ];
+
+  bool isEditing = false; // State untuk menentukan apakah sedang dalam mode editing
+
+  void toggleEditing() {
+    setState(() {
+      isEditing = !isEditing; // Toggle state editing saat tombol diklik
+    });
+  }
+
+  void addImage(String imagePath) {
+    setState(() {
+      images.add(imagePath); // Tambahkan gambar baru ke daftar gambar
+    });
+  }
+
+  Future<void> pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      File imageFile = File(pickedImage.path);
+      addImage(imageFile.path);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 80,
-              backgroundColor: Colors.grey,
-              child: IconButton(
-                icon: Icon(
-                  Icons.edit,
-                  color: Colors.white,
-                ),
-                onPressed: () {
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
                   // Logika ketika tombol edit foto ditekan
                 },
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Username'),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Alamat'),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Produk'),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Buka'),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: InkWell(
+                    onTap: toggleEditing, // Mengubah state saat tombol diklik
+                    child: Text(
+                      isEditing
+                          ? "Simpan"
+                          : "Edit Data", // Ubah teks tombol sesuai dengan state editing
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                        color: Colors.blue[700],
+                      ),
                     ),
                   ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Tutup'),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      enabled:
+                          isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      enabled:
+                          isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Alamat',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      enabled:
+                          isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Produk',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      enabled:
+                          isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Buka',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                        ),
+                        enabled:
+                            isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Tutup',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                        ),
+                        enabled:
+                            isEditing, // Aktifkan atau nonaktifkan input field sesuai dengan state editing
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              CarouselSlider(
+                items: [
+                  ...images.map(
+                    (image) {
+                      return Container(
+                        margin: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            image: AssetImage(image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  // Tambahkan widget "Tambah Foto" pada Carousel Slider
+                  InkWell(
+                    onTap: () {
+                      // Tambahkan logika ketika "Tambah Foto" diklik
+                      pickImage(); // Buka galeri untuk memilih foto
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.grey,
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ],
+                options: CarouselOptions(
+                  height: 200,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: true,
+                  viewportFraction: 0.8,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-           
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.remove('email');
-                await prefs.remove('password');
-                await prefs.remove('role');
-                prefs.setBool('isLoggedIn', false);
-                // Navigasi ke halaman login atau halaman lain yang sesuai
-                // ignore: use_build_context_synchronously
-                Navigator.pushNamed(context, LoginRole.nameRoute);
-              },
-              child: Text('Logout'),
-            ),
-          ],
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    await prefs.remove('email');
+                    await prefs.remove('password');
+                    await prefs.remove('role');
+                    prefs.setBool('isLoggedIn', false);
+                    Navigator.pushNamed(context, LoginRole.nameRoute);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text('Logout'),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
