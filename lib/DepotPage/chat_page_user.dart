78d3eb9a08edb1ecdd3408590b65844de4d93369
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-07-08 14:28:18 +0700

    memberikan bubble chat

--- lib/DepotPage/chat_page_user.dart
+++ lib/DepotPage/chat_page_user.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ChatPageUser extends StatefulWidget {
-  const ChatPageUser({super.key});
-  static const nameRoute = '/chatpageuser';
+  const ChatPageUser({Key? key}) : super(key: key);
+  static const String nameRoute = '/chatpageuser';
 
   @override
-  State<ChatPageUser> createState() => _ChatPageUser();
+  _ChatPageUserState createState() => _ChatPageUserState();
 }
 
-class _ChatPageUser extends State<ChatPageUser> {
+class _ChatPageUserState extends State<ChatPageUser> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  TextEditingController _messageController = TextEditingController();
+  List<Map<String, String>> _chatMessages = [];
+
+  void _sendMessage() {
+    String message = _messageController.text;
+
+    // Kirim logika pesan
+
+    // Tambahkan pesan baru ke daftar chatMessages
+    setState(() {
+      _chatMessages.add({'role': 'A', 'message': message});
+    });
+
+    // Tampilkan pop-up
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Pesan terkirim: $message'),
+      ),
+    );
+
+    // Bersihkan input teks
+    _messageController.clear();
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Chat'),
       ),
@@ -20,12 +51,21 @@ class _ChatPageUser extends State<ChatPageUser> {
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: _chatMessages.length,
               itemBuilder: (context, index) {
-                // Replace with actual chat messages
+                String role = _chatMessages[index]['role']!;
+                String message = _chatMessages[index]['message']!;
+
                 return ListTile(
-                  title: Text('Sender'),
-                  subtitle: Text('Message'),
+                  title: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text('Nama user', textAlign: TextAlign.right),
+                  ),
+                  subtitle: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(message, textAlign: TextAlign.right),
+                  ),
+                  contentPadding: EdgeInsets.fromLTRB(0, 0, 16, 0),
                 );
               },
             ),
@@ -47,8 +87,9 @@ class _ChatPageUser extends State<ChatPageUser> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: _messageController,
                     decoration: InputDecoration(
-                      hintText: 'Type a message...',
+                      hintText: 'Ketik pesan...',
                       border: InputBorder.none,
                     ),
                   ),
@@ -56,7 +97,7 @@ class _ChatPageUser extends State<ChatPageUser> {
                 IconButton(
                   icon: Icon(Icons.send),
                   onPressed: () {
-                    // Send message logic
+                    _sendMessage();
                   },
                 ),
               ],
